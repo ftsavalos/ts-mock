77cde2e3ffa9f6739bda29b35c646a2ed690d98a
Fotis Tsavalos <v-ftsavalos@eurobank.gr>
2018-10-04 11:13:09 +0300

    renamed ActiveMQConfig configuration class

---
+++ src/main/java/gr/eurobank/transactions/esbmock/config/ActiveMQConfig.java
@@ -0,0 +1,38 @@
+package gr.eurobank.transactions.esbmock.config;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsTemplate;
+
+import javax.jms.Queue;
+
+@Configuration
+public class ActiveMQConfig {
+
+    @Value("${spring.activemq.broker-url}")
+    private String brokerUrl;
+
+    @Value("${activemq.queue}")
+    private String destination;
+
+    @Bean
+    public Queue queue() {
+        return new ActiveMQQueue(destination);
+    }
+
+    @Bean
+    public ActiveMQConnectionFactory activeMQConnectionFactory() {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
+        factory.setBrokerURL(brokerUrl);
+        return factory;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate() {
+        return new JmsTemplate(this.activeMQConnectionFactory());
+    }
+
+}
